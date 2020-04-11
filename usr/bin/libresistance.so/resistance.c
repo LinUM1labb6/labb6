f6f1c11f12795e41bbe741130b4b7777123493da
Monika Willman <monika.willman@umu.se>
2020-04-11 23:00:25 +0200

    removed bin

--- usr/bin/libresistance.so/resistance.c
+++
@@ -1,8 +0,0 @@
-
-#include <stddef.h>
-#include <stdio.h>
-
-calc_resistance(int count, char conn, float *array) {
-  //TODO
-  return 0;
-}
--- usr/bin/libresistance.so/resistance.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RESISTANCE_H__
-#define __RESISTANCE_H__
-
-calc_resistance(int count, char conn, float *array);
-
-#endif
