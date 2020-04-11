e590b53ff5595be88fd8ae384e14b0a738f9d66f
Lucas <luclucsjoqvist@gmail.com>
2020-04-12 00:02:46 +0200

    add calc_power_i

--- usr/lib/libpower.so/power.c
+++ usr/lib/libpower.so/power.c
@@ -9,3 +9,12 @@ float calc_power_r(float volt, float resistance)
     effect = tmp / 100.0;
     return effect;
 }
+
+float calc_power_i(float volt, float current) 
+{
+    float effect;
+    effect = volt * current;
+    int temp = effect * 100;
+    effect = temp / 100.0;
+    return effect;
+}
