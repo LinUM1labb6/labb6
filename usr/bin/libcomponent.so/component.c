3bde1a24476a1003892c79349b43962f77e5a64a
Monika Willman <mowi62@gmail.com>
2020-04-11 09:29:58 +0200

    Update component.c

--- usr/bin/libcomponent.so/component.c
+++ usr/bin/libcomponent.so/component.c
@@ -1,9 +1,1 @@
-/* Auhtor : Mikael Troedsson, 2020 */
- 
-#ifndef __COMPONENT_H__
-#define __COMPONENT_H__
 
-int init_component(void);
-int e_resistance(float orig_resistance, float *res_array );
-		
-#endif
