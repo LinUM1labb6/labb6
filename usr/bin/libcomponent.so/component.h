e58f43a04b3e6217f2f8f4eb2ec2dc6b3a2ff813
Monika Willman <monika.willman@umu.se>
2020-04-11 09:45:00 +0200

    component.h made by Mikael Troedsson

--- usr/bin/libcomponent.so/component.h
+++ usr/bin/libcomponent.so/component.h
@@ -1,1 +1,13 @@
+/*
 
+file:	libcomponent.h
+​
+*/
+#ifndef __COMPONENT_H__
+#define __COMPONENT_H__
+​
+	int init_component(void);
+	int e_resistance(float orig_resistance, float *res_array );
+
+
+​
