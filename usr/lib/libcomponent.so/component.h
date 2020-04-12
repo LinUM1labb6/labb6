bdac10d52630485b94286108fd7b3ec0efce7b4c
Monika Willman <mowi62@gmail.com>
2020-04-13 00:20:40 +0200

    Delete component.h

--- usr/lib/libcomponent.so/component.h
+++
@@ -1,9 +0,0 @@
-/* AUthor : Mikael Troedsson */
-
-#ifndef __COMPONENT_H__
-#define __COMPONENT_H__
-
-int init_component(void);
-int e_resistance(float orig_resistance, float *res_array);
-
-#endif
